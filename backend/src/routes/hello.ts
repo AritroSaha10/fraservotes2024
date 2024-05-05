65f3b72bed0b5f52802c7e4fee500464239cf8c5
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2024-05-05 16:29:26 -0400

    remove random route

--- backend/src/routes/hello.ts
+++ backend/src/routes/hello.ts
@@ -1,28 +1,6 @@
 import { Request, Response } from 'express';
-import { getAuth } from "firebase-admin/auth";
 
 export function hello(req: Request, res: Response): void {
-  const listAllUsers = (nextPageToken?) => {
-    // List batch of users, 1000 at a time.
-    getAuth()
-      .listUsers(1000, nextPageToken)
-      .then((listUsersResult) => {
-        listUsersResult.users.forEach((userRecord) => {
-          console.log('user', userRecord.toJSON());
-        });
-        if (listUsersResult.pageToken) {
-          // List next batch of users.
-          listAllUsers(listUsersResult.pageToken);
-        }
-      })
-      .catch((error) => {
-        console.log('Error listing users:', error);
-      });
-  };
-  // Start listing users from the beginning, 1000 at a time.
-  listAllUsers();
-  
-    
-  res.send('hello world');
+    res.send('hello world');
 }
 
